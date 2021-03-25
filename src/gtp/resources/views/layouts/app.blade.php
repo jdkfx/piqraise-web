c59b741b7de3ba2790a8d1cf148199a737eb8e5d
jdkfx <amateurranch@gmail.com>
2021-03-25 17:12:42 +0900

    Add Tailwind CSS

---
+++ src/gtp/resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>GTP - @yield('title')</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Tailwind CSS -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+
+    <body>
+        <header>
+            @include('commons.navbar')
+        </header>
+
+        <main>
+            @yield('content')
+        </main>
+    </body>
+</html>
---
+++ src/gtp/resources/views/pages/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <p>ここが本文のコンテンツ</p>
+@endsection
