3adcad9ba67d3bf5eb24685ba17c764d29667bba
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2021-06-15 01:31:34 +0900

    add http status config file:wrench:

---
+++ src/piqraise/config/http.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'StatusOK' => 200,
+    'StatusCreated' => 201,
+    'StatusAccepted' => 202,
+    'StatusBadRequest' => 400,
+    'StatusNotFound' => 404,
+    'StatusInternalServerError' => 500
+];
