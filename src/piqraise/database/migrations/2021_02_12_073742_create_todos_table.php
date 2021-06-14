68bcc6db7ff535ef6cf752f293f180d29b1e585f
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2021-06-15 01:34:37 +0900

    add column deleted_at to todos table

--- src/piqraise/database/migrations/2021_02_12_073742_create_todos_table.php
+++ src/piqraise/database/migrations/2021_02_12_073742_create_todos_table.php
@@ -18,6 +18,7 @@ class CreateTodosTable extends Migration
             $table->integer('user_id')->unsigned()->index();
             $table->boolean('done_flag');
             $table->string('content');
+            $table->datetime('deleted_at');
             $table->timestamps();
         });
     }
