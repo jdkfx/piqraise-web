72532dc0c017024f658871dd2e95e31c075c59ee
yuu <uypsbss@gmail.com>
2021-07-24 23:34:27 +0900

    add public toggle design

--- src/piqraise/resources/views/pages/todo/create.blade.php
+++ src/piqraise/resources/views/pages/todo/create.blade.php
@@ -1,39 +1,35 @@
 @extends('layouts.app')
-
-@section('title', 'Home')
+@section('title', 'タスクの新規作成')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h1>ToDo作成</h1>
+    <div class="max-w-lg mx-auto px-2 sm:px-4">
+        <div class="my-12">
+            <div class="">
+                <h1>ToDo作成</h1>
 
-                        @if (count($errors) > 0)
-                            <div class="alert alert-danger">
-                                <ul class="text-danger alert alert-danger">
-                                    @foreach ($errors->all() as $error)
-                                        <li style="list-style: none;">{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-
-                        {!! Form::open(['route' => 'todo.create']) !!}
-
-                        {{ Form::token() }}
-                        <p>{{ Form::label('public_flag', '公開する場合はチェック') }}</p>
-                        <p>{{ Form::checkbox('public_flag', old('public_flag'), false, ['class' => 'form-control input_border']) }}</p>
-                        <p>{{ Form::label('content', '内容') }}</p>
-                        <p>{{ Form::textarea('content', old('content'), ['class' => 'form-control input_border', 'rows' => '8', 'cols' => '40']) }}</p>
-                        <p>{{ Form::label('target_date', '実行日') }}</p>
-                        <p>{{ Form::date('date', old('date'), ['class' => 'form-control input_border']) }}</p>
-
-                        {{ Form::submit('作成する', ['class' => 'back']) }}
-                        {!! Form::close() !!}
+                @if (count($errors) > 0)
+                    <div class="alert alert-danger">
+                        <ul class="text-danger alert alert-danger">
+                            @foreach ($errors->all() as $error)
+                                <li style="list-style: none;">{{ $error }}</li>
+                            @endforeach
+                        </ul>
                     </div>
-                </div>
+                @endif
+
+                {!! Form::open(['route' => 'todo.create']) !!}
+                    {{ Form::token() }}
+                    <p>{{ Form::label('content', '内容') }}</p>
+                    <p>{{ Form::textarea('content', old('content')) }}</p>
+                    <p>{{ Form::label('target_date', '実行日') }}</p>
+                    <p>{{ Form::date('date', old('date'), ['class' => 'form-control input_border']) }}</p>
+                    <div class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
+                        <input type="checkbox" name="public_flag" id="toggle" class="toggle-checkbox absolute block w-6 rounded-full bg-white border-white border-4 appearance-none cursor-pointer">
+                        <label for="toggle" class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
+                    </div>
+
+                    {{ Form::submit('作成する', ['class' => 'back']) }}
+                {!! Form::close() !!}
             </div>
         </div>
     </div>
