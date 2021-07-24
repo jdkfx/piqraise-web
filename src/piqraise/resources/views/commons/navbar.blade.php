df347b86791f513466a50d2b50f6d10d3ff808ce
yuu <uypsbss@gmail.com>
2021-07-24 20:48:34 +0900

    add login / signup button

--- src/piqraise/resources/views/commons/navbar.blade.php
+++ src/piqraise/resources/views/commons/navbar.blade.php
@@ -1,9 +1,33 @@
-<nav class="bg-green-800">
-  <div class="flex items-center justify-center px-2 h-16">
-    <div class="flex-shrink-0 flex items-center">
-      <a href="">
-        <h1 class="text-white text-xl">PiQraise</h1>
-      </a>
+<div class="bg-green-800">
+  <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+    <div class="relative flex items-center justify-between h-16">
+
+      <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+        <div class="flex-shrink-0 flex items-center">
+         <a href="">
+           <h1 class="text-white text-xl">PiQraise</h1>
+         </a>
+        </div>
+      </div>
+
+      <div class="inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+        <!-- Profile dropdown -->
+        <div class="ml-2">
+          <a href="">
+            <div class="px-4 py-1 rounded-lg bg-white">
+              <p class="text-green-900">Login</p>
+            </div>
+          </a>
+        </div>
+        <div class="ml-2">
+          <a href="">
+            <div class="px-4 py-1 rounded-lg bg-white">
+              <p class="text-green-900">Singup</p>
+            </div>
+          </a>
+        </div>
+      </div>
+
     </div>
   </div>
-</nav>
+</div>
