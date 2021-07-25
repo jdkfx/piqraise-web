85f129c481177d69fc76da83d1525fd0e93cc3e4
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2021-07-25 12:24:00 +0900

    fix

--- src/piqraise/app/Models/Todo.php
+++ src/piqraise/app/Models/Todo.php
@@ -14,9 +14,9 @@ class Todo extends Model
      *
      * @var string
      */
-    protected string $table = 'todos';
+    protected $table = 'todos';
 
-    protected array $fillable = ['user_id', 'done_flag', 'content', 'target_date'];
+    protected $fillable = ['user_id', 'done_flag', 'content', 'target_date'];
 
     public function user(): BelongsTo
     {
